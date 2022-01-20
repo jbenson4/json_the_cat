ad39db7200897fe82fee62c9c2133ac4d0f24eb2
Jordan <jordan.benson4@gmail.com>
2022-01-20 14:12:33 -0800

    Finished edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,7 @@
 const args = process.argv.slice(2);
 const request = require('request');
-const fs = require('fs');
 const breed = args[0];
 const apiUrl = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
-const workingUrl = 'https://api.thecatapi.com/v1/breeds/search?name=sphynx';
 
 const content = request(apiUrl, (error, response, body) => {
   if (error) {
@@ -12,6 +10,13 @@ const content = request(apiUrl, (error, response, body) => {
     return;
   }
   const data = JSON.parse(body);
+  if (data.message) {
+    console.log(data.message);
+    return;
+  }
+  if (data[0] === undefined) {
+    console.log('Breed not found. Please ensure proper spelling and try again.');
+    return;
+  }
   console.log(data[0].description);
-  
-})
+});
